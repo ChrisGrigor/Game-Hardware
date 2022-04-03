d1becc58ea197ae61df2693a4cd4b31aed5132f7
Chris <chrisgrigorsalas@gmail.com>
2022-04-03 17:54:58 -0400

    Raycast button checking

--- Unity Controller Prototype/Assets/ObjectClicker.cs
+++ Unity Controller Prototype/Assets/ObjectClicker.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ObjectClicker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                    PrintName(hit.transform.gameObject);
+            }
+
+        }
+    }
+
+    private void PrintName(GameObject go)
+    {
+        print(go.name);
+    }
+
+    private void Moveobject()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
